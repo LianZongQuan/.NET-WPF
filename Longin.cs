ba3db0c5825d5d88fef9fdca78e8be3b43126504
Simple-Nect <56957090+Simple-Nect@users.noreply.github.com>
2020-05-29 11:16:18 +0800

    Add files via upload

---
+++ Longin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employee_WPF.DAL;
+
+namespace Employee_WPF.BLL
+{
+    class Longin
+    {
+        SQLManger SQLManger = new SQLManger();
+        public bool longin(string str1, string str2)
+        {
+            return SQLManger.SQLlongin(str1,str2);
+        }
+    }
+}
---
+++ MangeBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employee_WPF.MODEL;
+using Employee_WPF.DAL;
+using Employee_WPF.UI;
+using Employee_WPF.BLL;
+namespace Employee_WPF.BLL
+{
+    class MangeBLL
+    {
+        public Salary search(List<string> list)
+        {
+            Salary salary = new Salary();
+            salary.Id = list[0];
+            salary.Name = list[1];
+            salary.Sex = list[2];
+            salary.Duty = list[3];
+            salary.Depart = list[4];
+            salary.Num1 = list[5];
+            salary.Num2 = list[6];
+            salary.Num3 = list[7];
+            salary.Num4 = list[8];
+            salary.image = list[9];
+            return salary;
+        }           
+    }
+}
